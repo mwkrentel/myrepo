7df1af0610fb595403e2ed8bdc862565342af8d1
Mark W. Krentel <krentel@rice.edu>
2019-09-05 17:02:16 -0500

    Add the simple.c unit test with an explicit call to gotcha_tool_init().
    Rename reduce-preinit to preinit.

--- gotcha/simple.c
+++ gotcha/simple.c
@@ -1,11 +1,12 @@
 /*
- *  Main program for openmp and libreduce.
+ *  Simple program for openmp without init constructor.  This tests if
+ *  the runtime library creates threads on its own.
  *
  *  Copyright (c) 2019, Rice University.
  *  See the file LICENSE for details.
  *
  *  Mark W. Krentel
- *  August 2019
+ *  September 2019
  */
 
 #include <sys/types.h>
@@ -20,13 +21,38 @@
 
 #define LIBM  "libm.so.6"
 
-double reduce(double *, int);
+typedef double sin_fcn_t (double);
+
+static sin_fcn_t * sin_fcn = NULL;
+
+void gotcha_tool_init(void);
+
+//----------------------------------------------------------------------
+
+double
+reduce(double A[], int N)
+{
+    double ans;
+    int i;
+
+    ans = 0.0;
+
+#pragma omp parallel for default(none) private(i)  \
+    shared(A, N, sin_fcn)  reduction(+ : ans)
+
+    for (i = 0; i < N; i++) {
+        ans += (* sin_fcn)(A[i]);
+    }
+
+    return ans;
+}
 
 int
 main(int argc, char **argv)
 {
     printf("main: first entry\n");
-
+    gotcha_tool_init();
+  
     int i, j, N;
 
     if (argc < 2 || sscanf(argv[1], "%d", &N) < 1) {
@@ -40,6 +66,16 @@ main(int argc, char **argv)
     }
 
     printf("main: calling dlopen() and sigprocmask() ...\n");
+
+    void * handle = dlopen(LIBM, RTLD_LAZY);
+    if (handle == NULL) {
+        err(1, "unable to dlopen(libm)");
+    }
+
+    sin_fcn = dlsym(handle, "sin");
+    if (sin_fcn == NULL) {
+        err(1, "unable to dlsym(sin)");
+    }
 
 #pragma omp parallel
     {
@@ -63,7 +99,6 @@ main(int argc, char **argv)
     for (j = 0; j < N; j++) {
 
 #pragma omp parallel for default(none) private(i) shared(A, N)
-
 	for (i = 0; i < N; i++) {
 	    A[i] = (double) i;
 	}
---
+++ gotcha/tool-init.c
@@ -0,0 +1,89 @@
+/*
+ *  gotcha_tool_init() -- call this explicitly right after main() from
+ *  within the application.
+ *
+ *  Normally, this is a separate library and compiled with GNU.
+ *
+ *  Copyright (c) 2019, Rice University.
+ *  See the file LICENSE for details.
+ *
+ *  Mark W. Krentel
+ *  August 2019
+ */
+
+#include <sys/types.h>
+#include <err.h>
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#include "gotcha/gotcha.h"
+
+typedef int (* pthread_create_fptr) (void *, void *, void *, void *);
+typedef int (* sigprocmask_fptr) (int, void *, void *);
+typedef void * (* dlopen_fptr) (char *, int);
+
+static gotcha_wrappee_handle_t pthread_create_handle;
+static gotcha_wrappee_handle_t sigprocmask_handle;
+static gotcha_wrappee_handle_t dlopen_handle;
+
+static pthread_create_fptr real_pthread_create;
+static sigprocmask_fptr real_sigprocmask;
+static dlopen_fptr real_dlopen;
+
+//----------------------------------------------------------------------
+
+static int
+pthread_create_wrap(void * thread, void * attr, void * start, void * arg)
+{
+    printf("---> gotcha: override: pthread_create\n");
+
+    int ret = real_pthread_create(thread, attr, start, arg);
+
+    return ret;
+}
+
+static int
+sigprocmask_wrap(int how, void * set, void * old_set)
+{
+    printf("---> gotcha: override: sigprocmask\n");
+
+    int ret = real_sigprocmask(how, set, old_set);
+
+    return ret;
+}
+
+static void *
+dlopen_wrap(char * name, int flags)
+{
+    printf("---> gotcha: override: dlopen\n");
+
+    void * ret = real_dlopen(name, flags);
+
+    return ret;
+}
+
+static gotcha_binding_t bindings [] = {
+    { "pthread_create", pthread_create_wrap, &pthread_create_handle },
+    { "sigprocmask", sigprocmask_wrap, &sigprocmask_handle },
+    { "dlopen", dlopen_wrap, &dlopen_handle },
+};
+
+void
+gotcha_tool_init(void)
+{
+    int i, n = sizeof(bindings)/sizeof(bindings[0]);
+
+    printf("---> gotcha: init ctor\n");
+
+    gotcha_wrap(bindings, n, "monitor");
+
+    real_pthread_create = (pthread_create_fptr) gotcha_get_wrappee(pthread_create_handle);
+    real_sigprocmask = (sigprocmask_fptr) gotcha_get_wrappee(sigprocmask_handle);
+    real_dlopen = (dlopen_fptr) gotcha_get_wrappee(dlopen_handle);
+
+    for (i = 0; i < n; i++) {
+	printf("---> gotcha: wrap:  %18s  -->  %p\n", bindings[i].name,
+	       (void *) gotcha_get_wrappee(*(bindings[i].function_handle)));
+    }
+}
